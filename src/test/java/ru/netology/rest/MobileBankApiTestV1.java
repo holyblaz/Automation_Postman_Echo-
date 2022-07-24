2262e1ae8dd05b24127ce52302b5ef8152146da1
pronin26 <elias.pronin@gmail.ru>
2022-07-24 18:21:14 +0300

    Method post checked

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -19,6 +19,6 @@ class MobileBankApiTestV1 {
       // Проверки
       .then()
           .statusCode(200)
-          .body("data", equalTo("some value"));
+          .body("data", equalTo("some data"));
     }
 }
